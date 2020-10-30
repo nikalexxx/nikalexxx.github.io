6bc8bc33e1025210b81d292f615bb077626e6c72
Alexandr Nikolaichev <saaacha.nik@yandex.ru>
2020-10-31 01:10:30 +0300

    add yandex metrika

--- html-gen.js
+++ html-gen.js
@@ -1,8 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const getHTML = version => {
-    const template = fs.readFileSync('index-template.html').toString();
-    return template.replace(/__VERSION__/g, version);
-}
+const metrikaCode = `<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(68829679, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/68829679" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->`;
 
-fs.writeFileSync('index.html', getHTML(process.argv[2]));
+const metrika = process.env.ENV === 'prod' ? metrikaCode : ''
+
+const getHTML = (version) => {
+    const template = fs.readFileSync("index-template.html").toString();
+    return template.replace(/%VERSION%/g, version).replace(/%METRIKA%/, metrika);
+};
+
+fs.writeFileSync("index.html", getHTML(process.argv[2]));
