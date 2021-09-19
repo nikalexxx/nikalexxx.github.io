0c2ede18e12645d628dbc7e4a39e0ea983c1990f
Alexandr Nikolaichev <saaacha.nik@yandex.ru>
2021-09-20 01:10:16 +0300

    build flow

--- deploy/run-build.ts
+++ deploy/run-build.ts
@@ -3,31 +3,38 @@ import ghpages from 'gh-pages';
 const root = `${__dirname}/..`;
 const targetBranch = 'build';
 
-const errorCallback = (error: any) => console.error(error);
+const errorCallback = (path: string, error: any) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('\x1b[32m%s\x1b[0m', `${path} done!`);
+    }
+};
+
+const getStep = (path: string, options: ghpages.PublishOptions) => (
+    callbacks?: (() => any)[]
+) => () => {
+    ghpages.publish(path, options, (error) => {
+        errorCallback(path, error);
+        if (!error) {
+            callbacks?.forEach((e) => e());
+        }
+    });
+};
 
 // собранные страницы
-ghpages.publish(
-    `${root}/dist`,
-    {
-        push: false,
-        branch: targetBranch,
-    },
-    (error) => {
-        errorCallback(error);
-        addData();
-    }
-);
+const addDist = getStep(`${root}/dist`, {
+    add: true,
+    push: true,
+    branch: targetBranch,
+});
 
-function addData() {
-    // данные
-    ghpages.publish(
-        `${root}/data`,
-        {
-            dest: 'data',
-            branch: targetBranch,
-            push: false,
-            add: true,
-        },
-        errorCallback
-    );
-}
+// данные
+const addData = getStep(`${root}/data`, {
+    dest: 'data',
+    branch: targetBranch,
+    push: false,
+});
+
+
+addData([addDist()])();
