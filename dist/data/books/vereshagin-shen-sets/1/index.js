d66cb1bfca6bf92a149577c504e0838fbfec32b6
Alexandr Nikolaichev <saaacha.nik@yandex.ru>
2021-09-22 22:19:06 +0300

    new build

---
+++ dist/data/books/vereshagin-shen-sets/1/index.js
@@ -0,0 +1,19 @@
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+import { getCustomElements } from '../elements.js';
+import paragraph1 from './1/index.js';
+import paragraph2 from './2/index.js';
+import paragraph3 from './3/index.js';
+import paragraph4 from './4/index.js';
+import paragraph5 from './5/index.js';
+import paragraph6 from './6/index.js';
+import paragraph7 from './7/index.js';
+import paragraph8 from './8/index.js';
+export default (function (api) {
+    var book = api.book;
+    var chapter = getCustomElements(api).chapter;
+    return book(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n"], ["\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n\n\n", "\n"])), chapter.key('sets')(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\u041C\u043D\u043E\u0436\u0435\u0441\u0442\u0432\u0430 \u0438 \u043C\u043E\u0449\u043D\u043E\u0441\u0442\u0438"], ["\u041C\u043D\u043E\u0436\u0435\u0441\u0442\u0432\u0430 \u0438 \u043C\u043E\u0449\u043D\u043E\u0441\u0442\u0438"]))), paragraph1(api), paragraph2(api), paragraph3(api), paragraph4(api), paragraph5(api), paragraph6(api), paragraph7(api), paragraph8(api));
+});
+var templateObject_1, templateObject_2;
